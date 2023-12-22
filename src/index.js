54281af09daf7347aa8f895a1a116025ca3f4a41
Hiếu Mai Xuân <hieumaixuan@Hieus-MacBook-Pro.local>
2023-12-22 19:35:10 +0700

    template engine (handlebars

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, '/resource/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.post('/', (req, res) => res.send('Post method execute...'));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})

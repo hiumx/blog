5ef00b2373ecf7d1185ac29f9a772f5253ce7d29
hiumx <hieumaixuan@Hieus-MacBook-Pro.local>
2023-12-23 00:13:57 +0700

    Default behavior FORM

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 import express from 'express';
- import path from 'path';
- import { engine } from 'express-handlebars';
- import morgan from 'morgan';
- import { fileURLToPath } from 'url';
+import path from 'path';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import { fileURLToPath } from 'url';
 
- const app = express();
- const port = 3000;
+const app = express();
+const port = 3000;
 
- const __filename = fileURLToPath(import.meta.url);
- const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
- app.use(morgan('combined'));
+app.use(morgan('combined'));
 
- app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
- app.engine('.hbs', engine({extname: '.hbs'}));
- app.set('view engine', '.hbs');
- app.set('views', path.join(__dirname, '/resource/views'));
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, '/resource/views'));
 
- app.get('/', (req, res) => {
+app.get('/', (req, res) => {
      res.render('home');
- })
+})
 
- app.post('/', (req, res) => res.send('Post method execute...'));
+app.get('/search', (req, res) => {
+     console.log(req.query)
+     res.render('search');
+})
 
- app.listen(port, () => {
-     console.log(`Example app listening on port ${port}`)});
+app.post('/search', (req, res) => {
+     console.log(req.query);
+     res.render('search')
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)});
